45a9ef9b3e487771c57c63727123ff3fee1881fb
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-12-26 14:37:53 +0200

    Add: password to user schema

--- apps/server/src/_schema/user.ts
+++ apps/server/src/_schema/user.ts
@@ -1,12 +1,15 @@
-import { sql } from 'drizzle-orm';
+import { sql, InferSelectModel } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   name: text('name', { length: 100 }).notNull(),
   email: text('email').notNull(),
+  password: text('password').notNull(),
   emailVerified: text('email_verified'),
   image: text('image'),
   createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`),
   updatedAt: text('updated_at').default(sql`CURRENT_TIMESTAMP`),
 });
+
+export type User = InferSelectModel<typeof user>;
