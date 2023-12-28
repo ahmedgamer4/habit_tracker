618e74acd3a5bb622727997b22d2fd35206578c8
ahmedgamer4 <ahmedalseidy4@gmail.com>
2023-12-28 12:07:55 +0200

    Add: activity log type

--- apps/server/src/_schema/activityLog.ts
+++ apps/server/src/_schema/activityLog.ts
@@ -1,5 +1,6 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { activity } from './activity';
+import { InferSelectModel } from 'drizzle-orm';
 
 export const activityLog = sqliteTable('activity_log', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
@@ -9,3 +10,5 @@ export const activityLog = sqliteTable('activity_log', {
   date: text('date'),
   count: integer('count'),
 });
+
+export type ActivityLog = InferSelectModel<typeof activityLog>;
